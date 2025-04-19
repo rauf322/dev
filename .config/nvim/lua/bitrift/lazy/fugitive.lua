ab381d47220d15bf0004ddd4179087eaf27fe74d
rauf322 <rauf.faizov.200271@gmail.com>
2025-04-19 08:47:08 +0200

    autocommit

--- .config/nvim/lua/bitrift/lazy/fugitive.lua
+++ .config/nvim/lua/bitrift/lazy/fugitive.lua
@@ -25,7 +25,7 @@ return {
                     vim.cmd.Git({'pull',  '--rebase'})
                 end, opts)
 
-                vim.keymap.set("n", "<leader>cm", ":Git commit -m ", opts);
+                vim.keymap.set("n", "<leader>cm", ":Git commit -m 'autocommit'", opts);
                 -- NOTE: It allows me to easily set the branch i am pushing and any tracking
                 -- needed if i did not set the branch up correctly
                 vim.keymap.set("n", "<leader>t", ":Git push -u origin ", opts);
